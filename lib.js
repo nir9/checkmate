1a88ffae916ed9d7ed041eb1bf889b88bd751e54
Nir Lichtman <nir_lichtman@hotmail.com>
2016-10-12 09:04:57 +0300

    Updates!

---
+++ lib.js
@@ -0,0 +1,12 @@
+function Point(row, col) {
+    this.row = -1;
+    this.col = -1;
+
+    if(row) {
+        this.row = row;
+    }
+
+    if(col) {
+        this.col = col;
+    }
+}
--- main.js
+++ main.js
@@ -7,37 +7,90 @@ var BISHOP = 'B';
 var KING = 'Ki';
 var QUEEN = 'Q';
 
-$(function() {
+var turn = 0;
+var pendingMove = new Point();
+
+$(function () {
     console.log("Loaded!");
-    setupBoard();    
+    initBoard();
+    setupBoard();
+    drawBoard();
 });
 
+/* also check to see if the move is ok */
+function doTurn(from, to) {
+    console.log("DOTURN;");
+    board[to.row][from.col] = board[from.row][from.col];
+    board[from.row][from.col] = EMPTY;
+    drawBoard();
+}
+
+function initBoard() {
+    board = [];
+    for (var i = 0; i < 8; i++) {
+        //board;
+        board[i] = [];
+
+        for (var c = 0; c < 8; c++) {
+            board[i].push(EMPTY);
+        }
+    }
+}
+
+function clickEvent(e) {
+    console.log("ASdSAD");
+    var row = $(this).parent('tr').index();
+    var col = $(this).index();
+
+    console.log(row, col);
+
+    //if(board[row][col] == EMPTY) {
+    if (pendingMove.col != -1) {
+        doTurn(pendingMove, new Point(row, col));
+        pendingMove = new Point();
+    } else {
+        console.log("asdasd");
+        pendingMove = new Point(row, col);
+    }
+    //}
+}
+
+function initEvents() {
+    $("#board tr td").click(clickEvent);
+}
+
 function setupBoard() {
-    for(var i = 0; i <= 8; i++) {
+    for (var i = 0; i <= 8; i++) {
         board[1][i] = PAWN;
         board[6][i] = PAWN;
     }
 
-    for(var i = 0; i<2; i++) {
-        board[i*7][0] = ROOK;
-        board[i*7][1] = KNIGHT;
-        board[i*7][2] = BISHOP;
-        board[i*7][3] = QUEEN;
-        board[i*7][4] = KING;
-        board[i*7][5] = BISHOP;
-        board[i*7][6] = KNIGHT;
-        board[i*7][7] = ROOK;
+    for (var i = 0; i < 2; i++) {
+        board[i * 7][0] = ROOK;
+        board[i * 7][1] = KNIGHT;
+        board[i * 7][2] = BISHOP;
+        board[i * 7][3] = QUEEN;
+        board[i * 7][4] = KING;
+        board[i * 7][5] = BISHOP;
+        board[i * 7][6] = KNIGHT;
+        board[i * 7][7] = ROOK;
     }
 
     console.log(board);
 }
 
-function initBoard() {
-    board = [];
-    for(var i = 0; i < 8; i++) {
-        //board;
-        for(var c = 0; c < 8; c++) {
-            board[i][c].push(EMPTY);
+function drawBoard() {
+    var content = "";
+
+
+    for (var i = 0; i < 8; i++) {
+        content += "<tr>";
+        for (var j = 0; j < 8; j++) {
+            content += "<td>" + board[i][j] + "</td>";
         }
+        content += "</tr>";
     }
+
+    $("#board").html(content);
+    initEvents();
 }
